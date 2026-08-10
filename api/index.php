3d145ba8de2ad593ba8c7ec68b834e6c3c66c317
muhamadrayhan23 <rayhanfaidasa023@gmail.com>
2026-08-10 23:10:44 +0700

    Fuck

--- api/index.php
+++ api/index.php
@@ -1,4 +1,19 @@
 <?php
 
-// Forward Vercel requests to Laravel index.php
+// Buat direktori temporary untuk Laravel di Vercel
+$storageFolders = [
+    '/tmp/storage/app',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($storageFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
