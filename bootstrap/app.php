07de46f5bfe238fac8b140e75608f32c6e49b4e1
muhamadrayhan23 <rayhanfaidasa023@gmail.com>
2026-08-10 20:49:42 +0700

    Change setup for vercel deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -19,3 +19,14 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+/*
+|--------------------------------------------------------------------------
+| Bind Storage Path for Vercel Serverless
+|--------------------------------------------------------------------------
+*/
+if (isset($_ENV['VERCEL_REGION'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
